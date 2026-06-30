52e9c6b458862b610c3ebe942aef9d7dd9899a91
Ayansh252yadav <ayanshy652@gmail.com>
2026-07-01 01:24:18 +0530

    add practice

---
+++ OnlinePlatform/StringSubString1967.java
@@ -0,0 +1,13 @@
+package OnlinePlatform;
+
+public class StringSubString1967 {
+    public int numOfStrings(String[] patterns, String word) {
+        int count=0;
+     for(String s:patterns){
+         if(word.contains(s)){
+             count++;
+         }
+     }
+     return count;
+    }
+}
